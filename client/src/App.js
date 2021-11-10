6d31882e6523d3d826cb4eeaac9001b26c183580
Humayon Zafar <humayonhere@gmail.com>
2021-11-10 23:42:32 +0500

    Completed Posts and comments flow.

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 //redux
 import {Provider} from "react-redux";
 import store from "./store";
@@ -20,6 +22,7 @@ import {loadUser} from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 //css
 import './App.css';
+
 
 
 if (localStorage.token) {
@@ -50,6 +53,8 @@ const App = () => {
                             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
                             <PrivateRoute exact path='/add-experience' component={AddExperience}/>
                             <PrivateRoute exact path='/add-education' component={AddEducation}/>
+                            <PrivateRoute exact path="/posts" component={Posts} />
+                            <PrivateRoute exact path="/posts/:id" component={Post} />
                         </Switch>
                     </section>
                 </Fragment>
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -14,6 +14,11 @@ const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
                 </Link>
             </li>
             <li>
+                <Link to='/posts'>
+                    Posts
+                </Link>
+            </li>
+            <li>
                 <Link to='/dashboard'>
                     <i className="fas fa-user"/>{' '}
                     <span className='hide-sm '>Dashboard</span></Link>
