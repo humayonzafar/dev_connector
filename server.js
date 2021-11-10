fc3cfcae259b741c31ffd689ac40299f3a40c0bf
Humayon Zafar <humayonhere@gmail.com>
2021-11-11 00:27:20 +0500

    added static folder and add production env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const httpsStatusCodes = require('./constants/httpStatusCodes');
+const path = require('path');
 
 const app = express();
 //connect database
@@ -8,14 +9,11 @@ connectDB();
 
 //init middleware
 app.use(express.json());                               //body json parsing
-app.use(express.urlencoded({extended:false})); //for form post requests
+app.use(express.urlencoded({extended: false})); //for form post requests
 
-app.get('/',(req,res)=>{
-    res.send('API Running...');
-})
 
 //set global variable
-app.use(function (req,res,next){
+app.use(function (req, res, next) {
     res.locals.httpsStatusCodes = httpsStatusCodes;
     next();
 });
@@ -25,6 +23,19 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
 
 const PORT = process.env.PORT || 5000;
 
